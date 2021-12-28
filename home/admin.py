3bc2f499c0258b931cee992964839194332dafc2
Ajeeshas41 <ajeeshas1992@gmail.com>
2021-12-28 12:02:36 +0530

    added home model configuration

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from home.models import Configuration
 
-# Register your models here.
+@admin.register(Configuration)
+class ConfigurationAdmin(admin.ModelAdmin):
+    fields = ('config','value')
+    empty_value_display = '-empty-'
--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Configuration(models.Model):
+    config = models.CharField(max_length=30)
+    value = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'configuration'
+    
+    def __str__(self) -> str:
+        return self.config
